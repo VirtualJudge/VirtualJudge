580e221b7557f6d832093ab644e4ec65f758c8c9
xudian <xudian.cn@gmail.com>
2018-03-11 00:14:56 +0800

    调整文件结构

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PyJudge
+# VirtualJudge
 基于Python爬虫的虚拟评测系统
--- VirtualJudge/urls.py
+++ VirtualJudge/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from PyJudge import views
+from VirtualJudge import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
