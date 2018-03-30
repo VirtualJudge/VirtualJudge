c3568a4cb0b8c539ab2368ba44b87368b1c0bb33
xudian <xudian.cn@gmail.com>
2018-03-31 01:08:18 +0800

    update init config

--- config/management/commands/init_user.py
+++ config/management/commands/init_user.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
             try:
                 User.objects.get(username='root')
             except ObjectDoesNotExist:
-                user = User.objects.create('root', 'root@vj.com', 'rootroot')
+                user = User.objects.create_superuser('root', 'root@vj.com', 'rootroot')
                 user.save()
         except Exception as e:
             self.stdout.write(self.style.ERROR('Failed to create user, error: ' + str(e)))
