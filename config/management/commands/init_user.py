be066c3c4a64b20f6532c09d474e9878fbec7910
xudian <xudian.cn@gmail.com>
2018-03-30 20:19:39 +0800

    update docker config

--- config/management/commands/init_user.py
+++ config/management/commands/init_user.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
-            user = User.objects.create_superuser('root', 'root@vj.com', 'rootroot')
-            user.save()
+            try:
+                User.objects.get(username='root')
+            except ObjectDoesNotExist:
+                user = User('root', 'root@vj.com', 'rootroot')
+                user.save()
         except Exception as e:
             self.stdout.write(self.style.ERROR('Failed to create user, error: ' + str(e)))
             exit(1)
