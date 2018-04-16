01c52554deecf5effeb1067f2d67645a272a2c55
prefixai <xudian.cn@gmail.com>
2018-04-17 03:52:03 +0800

    add setting to admin

--- remote/admin.py
+++ remote/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from remote.models import Setting
 from remote.models import Account, Language
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('oj_key', 'oj_value')
 
 
 class LanguageAdmin(admin.ModelAdmin):
@@ -9,7 +13,9 @@ class LanguageAdmin(admin.ModelAdmin):
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('oj_name', 'oj_username', 'oj_password', 'oj_account_status', 'update_time')
+    list_editable = ('oj_account_status')
 
 
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Setting, SettingAdmin)
