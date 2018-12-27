6f8265fd85ad82d05e139e668893abb2a6addd1a
prefixai <xudian.cn@gmail.com>
2018-12-27 23:36:09 +0800

    add index page accepted count

--- statistic/views.py
+++ statistic/views.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from django.utils import datetime_safe
 from rest_framework import status
 from rest_framework.views import APIView, Response
-
+from spider.config import Result
 from submission.models import Submission
 from utils.response import res_format
 
@@ -15,9 +15,11 @@ class SubmissionAPI(APIView):
         counts = []
         for offset in range(7, 0, -1):
             try:
-                counts.append(Submission.objects.filter(create_time__gte=today - timedelta(days=offset),
-                                                        create_time__lt=today - timedelta(days=offset - 1)).count())
-            except Exception as e:
-                print(e)
-                counts.append(0)
+                counts.append((Submission.objects.filter(create_time__gte=today - timedelta(days=offset),
+                                                         create_time__lt=today - timedelta(days=offset - 1)).count(),
+                               Submission.objects.filter(create_time__gte=today - timedelta(days=offset),
+                                                         create_time__lt=today - timedelta(days=offset - 1),
+                                                         verdict_code=Result.VerdictCode.VERDICT_ACCEPTED.value).count()))
+            except:
+                counts.append((0, 0))
         return Response(res_format(counts), status=status.HTTP_200_OK)
