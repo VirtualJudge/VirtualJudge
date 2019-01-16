9dc39872e04910cac1518cc65e179fce07f52869
prefixai <xudian.cn@gmail.com>
2019-01-16 22:07:02 +0800

    update client port

--- ws/client.py
+++ ws/client.py
@@ -3,10 +3,15 @@ from websocket import create_connection
 from VirtualJudge import settings
 import json
 
+if settings.production_env == 'production':
+    PORT = '9876'
+else:
+    PORT = '8000'
+
 
 class WebsocketClient:
     def __init__(self, chat_type, number):
-        url = f'ws://127.0.0.1:8000/api/ws_{chat_type}/{settings.SECRET_KEY}'
+        url = f'ws://127.0.0.1:{PORT}/api/ws_{chat_type}/{settings.SECRET_KEY}'
         self._ws = create_connection(url)
 
     def send(self, message):
@@ -18,7 +23,7 @@ class WebsocketClient:
 
 class SimpleWsClient:
     def __init__(self, chat_type, message):
-        url = f'ws://127.0.0.1:8000/api/ws_{chat_type}/{settings.SECRET_KEY}'
+        url = f'ws://127.0.0.1:{PORT}/api/ws_{chat_type}/{settings.SECRET_KEY}'
         self._ws = create_connection(url)
         self._ws.send(json.dumps({'message': message}))
         self._ws.close()
