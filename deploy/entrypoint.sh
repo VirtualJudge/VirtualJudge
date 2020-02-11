b068a450e73734444327140bdc17a29197e8b25e
dianhsu <xudian.cn@gmail.com>
2020-02-11 16:55:26 +0800

    add dockerfiles

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,17 @@
+APP=/app
+DATA=/data
+if [ ! -d "$DATA" ]; then
+  mkdir $DATA
+fi
+
+mkdir -p $data/log
+
+n=0
+while [ $n -lt 5 ]
+do
+    python3 manage.py migrate && break
+    n=$(($n+1))
+    echo "Failed to migrate, going to retry..."
+    sleep 8
+done
+exec ./manage.py runserver 0.0.0.0:8000
